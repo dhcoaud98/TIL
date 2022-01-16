f17fb57eb82064fbfd120f3ae08a8d5ba63c9a91
dhcoaud <dhcoaud98@gmail.com>
2022-01-16 23:43:58 +0900

    add

---
+++ hello.py
@@ -0,0 +1,26 @@
+#1
+#print ("안녕")
+#print ("안녕")
+#print ("안녕")
+#print ("안녕")
+#print ("안녕")
+
+#2
+greeting = '안녕'
+
+#print (greeting)
+#print (greeting)
+#print (greeting)
+#print (greeting)
+#print (greeting)
+
+#3 
+#for i in range(4):
+#    print(greeting)
+
+
+count = 0
+while count < 5:
+    print(greeting)
+    count += 1
+    break
