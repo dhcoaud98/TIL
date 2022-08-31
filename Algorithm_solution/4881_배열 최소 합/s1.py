49b5438c70a295379cbf79fc576c3f9e972e76c3
dhcoaud <dhcoaud98@gmail.com>
2022-08-31 15:03:20 +0900

    Algo_4881

--- Algorithm_solution/4881_배열 최소 합/s1.py
+++ Algorithm_solution/4881_배열 최소 합/s1.py
@@ -1,36 +1,30 @@
 import sys
 sys.stdin = open('sample_input.txt')
 
-def adjacent(x):  # x와 i 가 같으면 행이 같은거 근데 for문을 보면 x와 i가 같을 수가 없다.
-    for i in range(x):  # 인덱스가 행  row[n]값이 열
-        if row[x] == row[i]:  # 열이 같으면 false
-            return False
-    return True
-
-
-# 한줄씩 재귀하며 dfs 실행
-def dfs(x):
+def dfs(r, lst, ssum):
     global result
 
-    if x == N:
-        result += 1
+    if ssum >= result:  # 가지치기
+        return
+
+    if r == N:
+        if ssum <= result:
+            result = ssum
     else:
-        # 각 행에 퀸 놓기
-        for i in range(N):  # i 는 열번호 0부터 N 전까지 옮겨가면서 유망한곳 찾기
-            row[x] = i
-            if adjacent(x):  # 행,열 체크함수 true이면 백트래킹 안하고 계속 진행
-                dfs(x + 1)
+        for i in range(N):
+            if i not in visited:
+                lst.append(i)
+                dfs(r+1, lst, ssum+arr[r][i])
+                lst.pop()
+
 
 TC = int(input())
 for tc in range(1, TC+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    row = [0] * N
-    result = 0
-    print(row)
-    dfs(0)
-    # print(row)
-    print(result)
-
+    result = N*N*9
+    visited = []
+    dfs(0, visited, 0)
+    print(f'#{tc} {result}')
 
