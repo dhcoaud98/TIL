0f75edcac69b48e38c4d9930eb28a6990356b29d
dhcoaud <dhcoaud98@gmail.com>
2022-09-17 00:14:32 +0900

    사람네트워크2 pass

--- Algorithm_solution/1263_사람 네트워크2/main.py
+++ Algorithm_solution/1263_사람 네트워크2/main.py
@@ -1,23 +1,26 @@
 import sys
 sys.stdin = open('input.txt')
 
+def fc(n):
+    global answer
 
-def fc(x, nx):
-    """
-    :param x: 측정하고자 하는 시작점
-    :param nx: 현재 row
-    :return:
-    """
+    while queue:
 
-    for j in range(1, N+1):
-        if mat[nx][j] != 0 and j not in visited:
-            visited.append(j)
-            dy[x][j] = dy[x][nx] + 1
-            fc(x, j)
+        if sum(temp) > answer:  # 가지칙
+            break
+
+        p = queue.pop(0)
+        for k in range(1, N+1):
+            if mat[p][k] == 1 and k not in used:
+                temp[k] = temp[p] + 1
+                used.append(k)
+                queue.append(k)
+
+    if sum(temp) < answer:
+        answer = sum(temp)
 
 
 TC = int(input())
-# 다익스트라
 for tc in range(1, TC+1):
     arr = list(map(int, input().split()))
     N = arr[0]
@@ -25,27 +28,23 @@ for tc in range(1, TC+1):
     # print(arr)
 
     mat = [[0] * (N + 1)]
-    dy = [[0]*(N+1) for _ in range(N+1)]
 
     for n in range(N):
         next_row = [0] + arr[N*n: N*n+N]
         mat.append(next_row)
     # print(mat)
 
-    for i in range(1, N+1):
-        visited = []
-        visited.append(i)
-        fc(i, i)
-
-    # print(dy)
-
     answer = float('INF')
-    result = 0
 
-    for k in range(1, N+1):
-        # print(sum(dy[k]))
-        if sum(dy[k]) < answer:
-            answer = sum(dy[k])
-            result = k
+    for i in range(1, N+1):
+        queue = []
+        used = [i]
+        temp = [0] * (N+1)
+        for j in range(1, N+1):
+            if mat[i][j] == 1:
+                queue.append(j)
+                used.append(j)
+                temp[j] = 1
+        fc(i)
 
     print(f'#{tc} {answer}')
