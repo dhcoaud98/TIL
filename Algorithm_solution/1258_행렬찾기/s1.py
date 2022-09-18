177f9d72263113cd91b0b157fc0650e39d4d5b91
오채명 <dhcoaud98@naver.com>
2022-09-18 14:34:08 +0900

    SWEA_1264

--- Algorithm_solution/1258_행렬찾기/s1.py
+++ Algorithm_solution/1258_행렬찾기/s1.py
@@ -1,41 +1,52 @@
 import sys
 sys.stdin = open('input.txt')
 
+
+def dp(y, x):
+    global answer, used
+
+    for k in range(1, N):
+        if x+k < N and arr[y][x+k] != 0:
+            used.append([y, x+k])
+        else:
+            xlst.append(k)
+            while True:
+                for n in range(y+1, N):
+                    for m in range(x, x+k):
+                        if n < N and arr[n][m] != 0:
+                            used.append([n, m])
+                        else:
+                            ylst.append(n-y)
+                            return False
+
+            return
+
+
 TC = int(input())
-
-for tc in range(1, 11):
+for tc in range(1, TC+1):
     N = int(input())
-    arr = [list(map(int,input().split())) for _ in range(N)]
-    print(arr)
-    sub_matrix = 0
-    col_list = []
-    row_list = []
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    # print(arr)
+
+    xlst, ylst = [], []
+    answer = []
+    used = []
     for i in range(N):
-        col = row = now = 0
         for j in range(N):
-            if arr[i+now][j] != 0:
-                col += 1
-            else:
-                for k in range(N):
-                    if arr[i+now][j-1] != 0:
-                        row += 1
-                        now += 1
-                    else:
-                        break
+            # 0행 or 0열 인 경우
+            if arr[i][j] != 0 and [i, j] not in used:
+                used.append([i, j])
+                dp(i, j)
 
-        col_list.append(col)
-        row_list.append(row)
-    print(row_list, col_list)
+    for l in range(len(xlst)):
+        answer.append([xlst[l]*ylst[l], ylst[l], xlst[l]])
 
+    # print(ylst, xlst)
+    print(f'#{tc} {len(xlst)}', end=' ')
+    # print(answer)
+    answer.sort(key=lambda x: (x[0], x[1]))
+    # print(answer)
+    for a in answer:
+        print(a[1], a[2], end=' ')
+    print()
 
-
-
-
-
-    # if N <= 10:
-    #
-    # elif N < 40:
-    #     pass
-    # elif 40 < N <= 80:
-    #     pass
-    # elif 80 < N <=100:
